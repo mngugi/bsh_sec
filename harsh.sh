c19b97583a2180f2bb814e29514b434542ccc54b
mngugi <mngugi7@hotmail.com>
2025-09-25 21:25:56 +0300

    Update harsh.sh

--- harsh.sh
+++ harsh.sh
@@ -5,3 +5,11 @@
 hash_password() {
     echo -n "$1" | md5sum | awk '{print $1}'
 }
+
+if [$# -ne 2]; then
+    echo "Useage: <password> <dictionary_file>"
+    exit
+fi
+
+password=$1
+dictionary_file=$2
