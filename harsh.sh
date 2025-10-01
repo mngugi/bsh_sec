31cacd56b0964c811d4bf62ab54829f27e2e68d5
mngugi <mngugi7@hotmail.com>
2025-10-01 20:45:42 +0300

    Update harsh.sh

--- harsh.sh
+++ harsh.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Function to hash a password using MD5
-
 hash_password() {
     echo -n "$1" | md5sum | awk '{print $1}'
 }
 
-# Check if required arguments are provided 
-if [$# -ne 2]; then
-    echo "Useage: <password> <dictionary_file>"
-    exit
+# Check if required arguments are provided
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <password> <dictionary_file>"
+    exit 1
 fi
 
 password=$1
 dictionary_file=$2
+
